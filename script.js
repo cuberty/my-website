be2cd5442c3c0fe6f9eb341fa17b021c50880d47
Lee Yung Tat <109569734+cuberty@users.noreply.github.com>
2024-11-24 16:58:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').addEventListener('click', function() {
+        alert('Hello, welcome to my site!');
+    });
+});
